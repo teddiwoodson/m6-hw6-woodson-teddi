c1e9cb58d8aca1686538619387fdfef89dc8a13e
Teddi Woodson <teddiwood97@ufl.edu>
2019-09-28 15:32:43 -0400

    Add losing message to card 1 on click

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function cardChange() {
 for (i = 0; i < cards.length; i++){
   cards[i].addEventListener('mouseenter', cardChange);
 }
-
+//mouseleave fuctions
 function cardChangeBack() {
   for (i = 0; i < cards.length; i++){
     cards[i].classList.toggle('card');
@@ -19,3 +19,15 @@ for (i = 0; i < cards.length; i++){
   cards[i].addEventListener('mouseleave', cardChange);
 }
 //End all box hover
+
+//Oooh - so close, but no cigar
+var card1 = document.querySelector('.yellow');
+
+function noCigar() {
+  var heading = document.createElement("h2");
+  var heading_text = document.createTextNode("Oooh - so close, but no cigar");
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+  }
+
+card1.addEventListener('click', noCigar);
